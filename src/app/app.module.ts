5d6f535ed8f2416dbf02c57823966de8c7473fed
Joy <joyjk99@gmail.com>
2022-03-22 10:36:19 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+import {UserAuthModule} from './user-auth/user-auth.module';   
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDataComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-data/user-data.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-data',
+  templateUrl: './user-data.component.html',
+  styles: [
+    `.mStyle{color:red}`
+  ]
+})
+export class UserDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
