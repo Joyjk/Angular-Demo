c0e26e7858b4691144b7c4d7a9a89349cd2cea79
Joy <joyjk99@gmail.com>
2022-03-24 14:46:17 +0600

    PIPE added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,30 @@ export class AppComponent {
     console.log(item)
   }
 
+  arr:string[] = ["hello","avx"]
+
+  putData(data:number[])
+  {
+      return data[0]*34
+  }
+ 
+  getAll()
+  {
+      this.putData([21])
+  }
+
+  today = Date()
+
+  makeCaps(str: string)
+  {
+    return str.toUpperCase();
+  }
+
+  userDetails = [
+    {name: 'abc', address:'dhk', phone:'018', accounts:['facebook','gmail','yahoo']},
+    {name: 'def', address:'ctg', phone:'017', accounts:['facebook','gmail','yahoo']},
+    {name: 'ghi', address:'rhj', phone:'016', accounts:['facebook','gmail','yahoo']},
+    {name: 'jkl', address:'khu', phone:'015', accounts:['facebook','gmail','yahoo']},
+  ]
+
 }
